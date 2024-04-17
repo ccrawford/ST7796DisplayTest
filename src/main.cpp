3f5ab11b6b70ddf617fa24262abf8d0a57f04014
Chris Crawford <chris@ccrawford.org>
2024-04-17 14:06:05 -0500

    Removed unnecessary sprites and created a double buffer. Some naming inconsistencies.

--- src/main.cpp
+++ src/main.cpp
@@ -16,73 +16,70 @@
 //====================================================================================
 //                                  Libraries
 //====================================================================================
-// Call up the SPIFFS FLASH filing system, this is part of the ESP Core
-#define FS_NO_GLOBALS
-#include "LittleFS.h"
+#define DISABLE_ALL_LIBRARY_WARNINGS
+
+#include <Arduino.h>
+
 #include "dial_background.h"
-#include "turn_background.h"
-#include "ball_background.h"
-#include "lights_bg.h"
-#include "ready_light.h"
 #include "ball.h"
 #include "plane_outline.h"
-#include "over_write.h"
+#include "LED_Images.h"
 
-#include <TFT_eSPI.h>                  // Hardware-specific library
-TFT_eSPI tft = TFT_eSPI();             // Invoke custom library
+#include <TFT_eSPI.h>      // Hardware-specific library
+TFT_eSPI tft = TFT_eSPI(); // Invoke custom library
 
 // Sprites used in display
-TFT_eSprite planeSpr = TFT_eSprite(&tft);   // Plane sprite
-TFT_eSprite planeBgSpr = TFT_eSprite(&tft);      // Plane Background sprite
-TFT_eSprite ballSpr = TFT_eSprite(&tft);    // Turn coordinator ball sprite
-TFT_eSprite ballBgSpr = TFT_eSprite(&tft);  // Turn coordinator ball background sprite
-TFT_eSprite lightsBgSpr = TFT_eSprite(&tft); 
-TFT_eSprite readyLightSpr = TFT_eSprite(&tft); 
+TFT_eSprite mainSpr = TFT_eSprite(&tft);   // Main sprite. Full screen.
 
+// The plane and the ball need to be sprites so they can be rotated and masked.
+TFT_eSprite planeSpr = TFT_eSprite(&tft);   // Plane sprite
+TFT_eSprite ballSpr = TFT_eSprite(&tft);    // Turn coordinator ball sprite
+
+// The LEDs can just be overlayed.
+
+// Create image buffers for double buffering
+#define INSTRUMENT_WIDTH 320
+#define INSTRUMENT_HEIGHT 300
 
 //====================================================================================
 //                                    Setup
 //====================================================================================
 void setup()
 {
-  Serial.begin(115200); 
+  Serial.begin(115200);
   while (!Serial)
-    ;          
+    ;
   delay(1000); // wait for serial monitor to open
   Serial.println("\r\nTurn Coordinator PoC\n");
-  
+
+  tft.initDMA(); // Init DMA engine to speed up rendering
+
   tft.begin();
   tft.setRotation(0); // 0 & 2 Portrait. 1 & 3 landscape
-  tft.setSwapBytes(true);
-  tft.pushImage(0, 0, dialWidth, dialHeight, dial);
+  // tft.setSwapBytes(true);
+  //tft.pushImage(0, 0, dialWidth, dialHeight, dial);
+
+  mainSpr.createSprite(INSTRUMENT_WIDTH, INSTRUMENT_HEIGHT);
+  mainSpr.setSwapBytes(true);
+  mainSpr.pushImage(0, 0, dialWidth, dialHeight, dial);
 
   // Create a sprite to hold the jpeg (or part of it)
   // CAC set impage depth.
-  planeSpr.setColorDepth(16);
+  //planeSpr.setColorDepth(16);
   planeSpr.createSprite(planeOutlineWidth, planeOutlineHeight);
-  planeSpr.setPivot(83, 22);  // Determined in paint program. Plane rotates around the center of the fuselage.
+  planeSpr.setPivot(83, 22); // Determined in paint program. Plane rotates around the center of the fuselage.
   planeSpr.setSwapBytes(true);
   planeSpr.pushImage(0, 0, planeOutlineWidth, planeOutlineHeight, planeOutline);
 
-  planeBgSpr.createSprite(turnOverWriteWidth, turnOverWriteHeight);
-  planeBgSpr.setSwapBytes(true);
-  planeBgSpr.setPivot(91, 50); // Where the plane's pivot point is on the background sprite.
-
-  ballSpr.createSprite(ballWidth, ballHeight);    
+  ballSpr.createSprite(ballWidth, ballHeight);
   ballSpr.pushImage(0, 0, ballWidth, ballHeight, ball_image);
   // The ball does not pivot, only moves x/y
 
-  ballBgSpr.createSprite(ballBackgroundwidth, ballBackgroundHeight);
-  ballBgSpr.setSwapBytes(true);
 
-  lightsBgSpr.createSprite(LightsBgWidth, LightsBgHeight);
-  lightsBgSpr.setSwapBytes(true);
-  
-  readyLightSpr.createSprite(readyDotWidth, readyDotHeight);
-  readyLightSpr.pushImage(0, 0, readyDotWidth, readyDotHeight, ReadyDot);
+  // readyLightSpr.createSprite(readyDotWidth, readyDotHeight);
+  // readyLightSpr.pushImage(0, 0, readyDotWidth, readyDotHeight, ReadyDot);
 
   Serial.println("\r\nInitialisation done.\r\n");
-  
 }
 
 //====================================================================================
@@ -91,39 +88,41 @@ void setup()
 void loop()
 {
 
- 
-  for (int16_t angle = -30; angle <= 30; angle += 1)
+  for (int16_t i = -60; i <= 60; i++)
   {
-    planeBgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);   // Put a fresh background in the changing area to overwrite old plane.
-    planeSpr.pushRotated(&planeBgSpr, angle, TFT_WHITE);  // Push the plane sprite on the background
-    planeBgSpr.pushSprite(69, 96);  // Push the background with the plane to the screen at appropriate spot. Coicicental coordinates!
+    int16_t angle = abs(i) - 30;
 
-    ballBgSpr.pushImage(0, 0, ballBackgroundwidth, ballBackgroundHeight, ball_background);   // Put a fresh background in the changing area to overwrite old plane.
-    ballSpr.pushToSprite(&ballBgSpr, ballBgSpr.width()/2 + angle - 10, ballBgSpr.height() - ballSpr.height() - round((abs(angle)/5)) + 0, TFT_WHITE);  // Push the plane sprite on the background
-    ballBgSpr.pushSprite(97,236);  // Push the background with the plane to the screen at appropriate spot. Coicicental coordinates!
+    mainSpr.pushImage(0, 0, dialWidth, dialHeight, dial); // Put a fresh background in the changing area to overwrite old plane.
 
-    lightsBgSpr.pushImage(0, 0, LightsBgWidth, LightsBgHeight, LightsBg);   // Put a fresh background in the changing area to overwrite old lights
-    if(angle < 10)
+    ballSpr.pushToSprite(&mainSpr, mainSpr.width() / 2 + angle - 10, mainSpr.height() - ballSpr.height() - round((abs(angle) / 5)) - 24, TFT_WHITE); 
+    
+    if (angle == 0) delay(2000);
+
+    if (angle < 10)
     {
-    readyLightSpr.pushToSprite(&lightsBgSpr, 1, 49, TFT_WHITE);  // Push the LED sprite on the Lights background
+      mainSpr.pushImage(STDotX, STDotY, APDot_width, APDot_height, APDot);
+      mainSpr.pushImage(HDDotX, HDDotY, APDot_width, APDot_height, APDot);
+      mainSpr.pushImage(TrkLoDotX, TrkLoDotY, APDot_width, APDot_height, APDot);
+      mainSpr.pushImage(TrkHiDotX, TrkHiDotY, APDot_width, APDot_height, APDot);
+
+      mainSpr.pushImage(AltDot_x, AltDot_y, AltDot_width, AltDot_height, AltDot);
+
+      mainSpr.pushImage(UpDot_x, UpDot_y, UpDot_width, UpDot_height, UpDot);
+      mainSpr.pushImage(DownDot_x, DownDot_y, DownDot_width, DownDot_height, DownDot);
+
+      mainSpr.pushImage(ReadyDot_x, ReadyDot_y, ReadyDot_width, ReadyDot_height, ReadyDot);
+
+      mainSpr.pushImage(LowVoltFlag_x, LowVoltFlag_y, LowVoltFlag_width, LowVoltFlag_height, LowVoltFlag);
+
     }
-    lightsBgSpr.pushSprite(lightsBgX,lightsBgY);  // Push the background with the plane to the screen at appropriate spot. Coicicental coordinates!
 
-    delay(50);
+    // Do the plane last. It's on top of all the others.
+    mainSpr.setPivot(160, 150);                             // Set the pivot point for the rotation of the background
+    planeSpr.pushRotated(&mainSpr, angle, TFT_WHITE);      // Push the plane sprite on the background
+
+    mainSpr.pushSprite(0, 0); // Push the main sprite to the screen
+    delay(10);
+ 
   }
-  for (int16_t angle = 30; angle >= -30; angle -= 1)
-  {
-  planeBgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);
-    planeSpr.pushRotated(&planeBgSpr, angle, TFT_WHITE);
-    planeBgSpr.pushSprite(69,96);
-
-    ballBgSpr.pushImage(0, 0, ballBackgroundwidth, ballBackgroundHeight, ball_background);   // Put a fresh background in the changing area to overwrite old plane.
-    ballSpr.pushToSprite(&ballBgSpr, ballBgSpr.width()/2 + angle - 10, ballBgSpr.height() - ballSpr.height() - round((abs(angle)/5)) + 0, TFT_WHITE);  // Push the plane sprite on the background
-    ballBgSpr.pushSprite(97,236);  // Push the background with the plane to the screen at appropriate spot. Coicicental coordinates!
-
-    delay(50);
-  }
-
-  delay(2000);
 
 }
