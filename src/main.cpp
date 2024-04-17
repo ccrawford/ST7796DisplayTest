1d00e016e97ab6efc5cd791085e713256e09eedb
Chris Crawford <chris@ccrawford.org>
2024-04-17 10:57:58 -0500

    Refactor and cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,6 @@
-/*====================================================================================
 
-  This example draws a jpeg image in a Sprite then plot a rotated copy of the Sprite
-  to the TFT.
-
-  The jpeg used in in the sketch Data folder (press Ctrl+K to see folder)
-
-  The jpeg must be uploaded to the ESP8266 or ESP32 SPIFFS by using the Tools menu
-  sketch data upload option of the Arduino IDE. If you do not have that option it can
-  be added. Close the Serial Monitor window before uploading to avoid an error message!
-
-  To add the upload option for the ESP8266 see:
-  http://www.esp8266.com/viewtopic.php?f=32&t=10081
-  https://github.com/esp8266/arduino-esp8266fs-plugin/releases
-
-  To add the upload option for the ESP32 see:
-  https://github.com/me-no-dev/arduino-esp32fs-plugin
-
-  Created by Bodmer 6/1/19 as an example to the TFT_eSPI library:
+/*
+  Based on Bodmer's library:
   https://github.com/Bodmer/TFT_eSPI
 
   Extension functions in the TFT_eFEX library are used to list SPIFFS files and render
@@ -43,108 +27,61 @@
 #include "ball.h"
 #include "plane_outline.h"
 #include "over_write.h"
-#include <FS.h>
 
-// https://github.com/Bodmer/TFT_eSPI
 #include <TFT_eSPI.h>                  // Hardware-specific library
 TFT_eSPI tft = TFT_eSPI();             // Invoke custom library
-TFT_eSprite spr = TFT_eSprite(&tft);   // Create Sprite object "spr" with pointer to "tft" object
-TFT_eSprite bgSpr = TFT_eSprite(&tft); // Create Sprite object "spr" with pointer to "tft" object
-TFT_eSprite ballBgSpr = TFT_eSprite(&tft); // Create Sprite object "spr" with pointer to "tft" object
-TFT_eSprite ballSpr = TFT_eSprite(&tft); // Create Sprite object "spr" with pointer to "tft" object
-TFT_eSprite lightsBgSpr = TFT_eSprite(&tft); // Create Sprite object "spr" with pointer to "tft" object
-TFT_eSprite readyLightSpr = TFT_eSprite(&tft); // Create Sprite object "spr" with pointer to "tft" object
 
-// https://github.com/Bodmer/TFT_eFEX
-#include <TFT_eFEX.h>          // Include the function extension library
-TFT_eFEX fex = TFT_eFEX(&tft); // Create TFT_eFX object "fex" with pointer to "tft" object
+// Sprites used in display
+TFT_eSprite planeSpr = TFT_eSprite(&tft);   // Plane sprite
+TFT_eSprite planeBgSpr = TFT_eSprite(&tft);      // Plane Background sprite
+TFT_eSprite ballSpr = TFT_eSprite(&tft);    // Turn coordinator ball sprite
+TFT_eSprite ballBgSpr = TFT_eSprite(&tft);  // Turn coordinator ball background sprite
+TFT_eSprite lightsBgSpr = TFT_eSprite(&tft); 
+TFT_eSprite readyLightSpr = TFT_eSprite(&tft); 
+
 
 //====================================================================================
 //                                    Setup
 //====================================================================================
 void setup()
 {
-  Serial.begin(115200); // Used for messages
+  Serial.begin(115200); 
   while (!Serial)
-    ;          // Wait for Serial to be ready
+    ;          
   delay(1000); // wait for serial monitor to open
-  Serial.println("\r\nRotated Sprite 3 example\n");
+  Serial.println("\r\nTurn Coordinator PoC\n");
+  
   tft.begin();
   tft.setRotation(0); // 0 & 2 Portrait. 1 & 3 landscape
   tft.setSwapBytes(true);
-  bgSpr.setSwapBytes(true);
-  ballBgSpr.setSwapBytes(true);
-  // ballSpr.setSwapBytes(true);
-  tft.fillScreen(TFT_BLACK);
-
-  LittleFSConfig cfg;
-  cfg.setAutoFormat(false);
-  LittleFS.setConfig(cfg);
-  LittleFS.begin();
-  // File f = LittleFS.open("/Eye_80x64.jpg", "r");
+  tft.pushImage(0, 0, dialWidth, dialHeight, dial);
 
   // Create a sprite to hold the jpeg (or part of it)
   // CAC set impage depth.
-  spr.setColorDepth(16);
-  spr.createSprite(166, 33);
-  bgSpr.createSprite(turnOverWriteWidth, turnOverWriteHeight);
+  planeSpr.setColorDepth(16);
+  planeSpr.createSprite(planeOutlineWidth, planeOutlineHeight);
+  planeSpr.setPivot(83, 22);  // Determined in paint program. Plane rotates around the center of the fuselage.
+  planeSpr.setSwapBytes(true);
+  planeSpr.pushImage(0, 0, planeOutlineWidth, planeOutlineHeight, planeOutline);
+
+  planeBgSpr.createSprite(turnOverWriteWidth, turnOverWriteHeight);
+  planeBgSpr.setSwapBytes(true);
+  planeBgSpr.setPivot(91, 50); // Where the plane's pivot point is on the background sprite.
+
+  ballSpr.createSprite(ballWidth, ballHeight);    
+  ballSpr.pushImage(0, 0, ballWidth, ballHeight, ball_image);
+  // The ball does not pivot, only moves x/y
 
   ballBgSpr.createSprite(ballBackgroundwidth, ballBackgroundHeight);
-  ballSpr.createSprite(ballWidth, ballHeight);
+  ballBgSpr.setSwapBytes(true);
 
   lightsBgSpr.createSprite(LightsBgWidth, LightsBgHeight);
   lightsBgSpr.setSwapBytes(true);
+  
   readyLightSpr.createSprite(readyDotWidth, readyDotHeight);
-  // readyLightSpr.setSwapBytes(true);
+  readyLightSpr.pushImage(0, 0, readyDotWidth, readyDotHeight, ReadyDot);
 
   Serial.println("\r\nInitialisation done.\r\n");
-
-  // Lists the files so you can see what is in the SPIFFS
-  // fex.listSPIFFS();
-
-  Dir dir = LittleFS.openDir("/");
-  Serial.printf("File size: %d\n", dir.fileSize());
-  while (dir.next())
-  {
-    Serial.print("File: ");
-    Serial.print(dir.fileName());
-    if (dir.fileSize())
-    {
-      File f = dir.openFile("r");
-      Serial.println(f.size());
-    }
-  }
-
-  // Note the / before the SPIFFS file name must be present, this means the file is in
-  // the root directory of the SPIFFS, e.g. "/tiger.jpg" for a file called "tiger.jpg"
-
-  // Send jpeg info to serial port
-  fex.jpegInfo("/plane_outline.jpg");
-  // fex.jpegInfo("/turn_coord_sprite.jpg");
-
-  // Draw jpeg image in Sprite spr at 0,0
-  // fex.drawJpeg("/turn_coord_sprite.jpg", 0 , 0, &spr);
-  // fex.drawJpeg("/dial_background.jpg", 0 , 0, &spr);
-  fex.drawBmp("/plane_outline.bmp", 0, 0, &spr);
-  // fex.drawJpeg("/plane_outline.jpg", 0, 0, &spr);
-  // tft.pushImage(0,0, planeBmpWidth, planeBmpHeight, plane_outline,  TFT_BLACK, cmap &spr);
-
-  //bgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);
-  //  tft.fillScreen(TFT_DARKGREY);
-  //  fex.drawJpeg("/dial_background.jpg", 0 , 0);
-  tft.pushImage(0, 0, dialWidth, dialHeight, dial);
-
-  // Set the TFT pivot point to the centre of the screen
-  //tft.setPivot(160, 148);
-
-  bgSpr.setPivot(90, 45); // Center of the background.
-
-  // Set Sprite pivot point to centre of Sprite
-  spr.setPivot(spr.width() / 2, spr.height() / 2);
-  ballBgSpr.setPivot(ballBgSpr.width()/2, 0);
-
-  ballSpr.pushImage(0, 0, ballWidth, ballHeight, ball_image);
-  readyLightSpr.pushImage(0, 0, readyDotWidth, readyDotHeight, ReadyDot);
   
 }
 
@@ -157,9 +94,9 @@ void loop()
  
   for (int16_t angle = -30; angle <= 30; angle += 1)
   {
-    bgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);   // Put a fresh background in the changing area to overwrite old plane.
-    spr.pushRotated(&bgSpr, angle, TFT_BLACK);  // Push the plane sprite on the background
-    bgSpr.pushSprite(69, 96);  // Push the background with the plane to the screen at appropriate spot. Coicicental coordinates!
+    planeBgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);   // Put a fresh background in the changing area to overwrite old plane.
+    planeSpr.pushRotated(&planeBgSpr, angle, TFT_WHITE);  // Push the plane sprite on the background
+    planeBgSpr.pushSprite(69, 96);  // Push the background with the plane to the screen at appropriate spot. Coicicental coordinates!
 
     ballBgSpr.pushImage(0, 0, ballBackgroundwidth, ballBackgroundHeight, ball_background);   // Put a fresh background in the changing area to overwrite old plane.
     ballSpr.pushToSprite(&ballBgSpr, ballBgSpr.width()/2 + angle - 10, ballBgSpr.height() - ballSpr.height() - round((abs(angle)/5)) + 0, TFT_WHITE);  // Push the plane sprite on the background
@@ -176,9 +113,9 @@ void loop()
   }
   for (int16_t angle = 30; angle >= -30; angle -= 1)
   {
-  bgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);
-    spr.pushRotated(&bgSpr, angle, TFT_BLACK);
-    bgSpr.pushSprite(69,96);
+  planeBgSpr.pushImage(0, 0, turnOverWriteWidth, turnOverWriteHeight, turnOverwrite);
+    planeSpr.pushRotated(&planeBgSpr, angle, TFT_WHITE);
+    planeBgSpr.pushSprite(69,96);
 
     ballBgSpr.pushImage(0, 0, ballBackgroundwidth, ballBackgroundHeight, ball_background);   // Put a fresh background in the changing area to overwrite old plane.
     ballSpr.pushToSprite(&ballBgSpr, ballBgSpr.width()/2 + angle - 10, ballBgSpr.height() - ballSpr.height() - round((abs(angle)/5)) + 0, TFT_WHITE);  // Push the plane sprite on the background
